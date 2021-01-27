f6586f42bfcb3fb7267e37d124306776ade2244d
garvitbhardwaj2001 <49810231+garvitbhardwaj2001@users.noreply.github.com>
2021-01-27 23:00:18 +0530

    Update A1Q1_one_bit_full_adder.v

--- A1/A1Q1_one_bit_full_adder.v
+++ A1/A1Q1_one_bit_full_adder.v
@@ -9,7 +9,7 @@ module one_bit_full_adder (a, b, cin, sum, cout);
    output cout;
    wire cout;
 
-   assign sum=a^b^cin;
-   assign cout=((a&b) | (b&cin) | (a&cin));
+   assign sum=a^b^cin;//^ denotes XOR
+   assign cout=((a&b) | (b&cin) | (a&cin));// if any two of a, b, cin are 1 then cout is 1
 
 endmodule
