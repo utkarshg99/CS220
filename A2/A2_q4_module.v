563ac8285be37c83d41c3b500de57928eb640cac
garvitbhardwaj2001 <49810231+garvitbhardwaj2001@users.noreply.github.com>
2021-02-02 14:18:07 +0530

    Add files via upload

--- A2/A2_q4_module.v
+++ A2/A2_q4_module.v
@@ -1,27 +1,18 @@
 module M(clk, y);
 
-input  clk; output reg [3:0] y=4'b1;
+input  clk; output reg [3:0] y=8;
 
 
 reg [15:0] count = 16'b0;
 
 always @(posedge clk) begin
     count <= count + 16'b1;
-  if ((y==4'b1)&&(count==25000)) begin
+  if (count==25000) begin
     count <= 16'b1;
-    y  <=   2;
+    y[3]  <=   y[2];
+    y[2]  <=   y[1];
+    y[1]  <=   y[0];
+    y[0]  <=   y[3];
   end
-else if((y == 2)&&(count == 25000)) begin
-    y  <= 4;
-    count <= 16'b1;
-end
-else if((y == 4)&&(count == 25000)) begin
-    y  <= 8;
-    count <= 16'b1;
-end
-else if((y == 8)&&(count == 25000)) begin
-    y  <= 1;
-    count <= 16'b1;
-end
   end
 endmodule
